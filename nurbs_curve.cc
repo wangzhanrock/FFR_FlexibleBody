76a9e6ca230488c4f2ab5de6e1e068d088d337a9
wangzhanrock <wangzhanrock@gmail.com>
2015-01-05 00:00:40 +0100

    add nurbs curve file

---
+++ nurbs_curve.cc
@@ -0,0 +1,5 @@
+#include <nurbs.h>
+
+Nurbs::Nurbs()  {
+  std::cout << "Hi" << std::endl;
+}
---
+++ nurbs_curve.h
@@ -0,0 +1,13 @@
+#ifndef NURBS_H_
+#define NURBS_H
+
+class Nurbs {
+  public:
+    Nurbs();
+
+    virtual ~Nurbs();
+
+};
+
+
+#endif
